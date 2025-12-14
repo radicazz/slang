26ce909eb249123ad338565bae7b8c1da1e717af
cazz <radicazz.dev@gmail.com>
2025-12-14 13:10:53 -0500

    feat: add audio subsystem initialization

---
+++ src/helpers/audio.h
@@ -0,0 +1,31 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <SDL3/SDL_audio.h>
+
+typedef enum {
+    SOUND_EAT_FOOD,
+    SOUND_COUNT  // Must be last
+} sound_id_t;
+
+typedef struct {
+    uint8_t* buffer;
+    size_t length;
+    SDL_AudioSpec spec;
+} sound_data_t;
+
+typedef struct {
+    SDL_AudioStream* stream;
+    sound_data_t sounds[SOUND_COUNT];
+    bool is_initialized;
+} audio_manager_t;
+
+bool audio_manager_create(audio_manager_t* manager);
+void audio_manager_destroy(audio_manager_t* manager);
+
+bool audio_manager_load_sound(audio_manager_t* manager, sound_id_t id, const char* filepath);
+bool audio_manager_play_sound(audio_manager_t* manager, sound_id_t id);
+
+#endif  // AUDIO_H
