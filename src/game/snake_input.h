1d5e27ae256d00484d06fcc0a43006c7802ced7c
cazz <radicazz.dev@gmail.com>
2026-01-09 22:18:57 +0200

    Revert "refactor: split snake into modules"
    This reverts commit f02d862debb4c6e8265d620843706cd1064a73eb.

--- src/game/snake_input.h
+++
@@ -1,8 +0,0 @@
-#ifndef SNAKE_INPUT_H
-#define SNAKE_INPUT_H
-
-#include "../snake.h"
-
-void snake_handle_events(snake_t* snake);
-
-#endif  // SNAKE_INPUT_H
--- src/game/snake_render.h
+++
@@ -1,8 +0,0 @@
-#ifndef SNAKE_RENDER_H
-#define SNAKE_RENDER_H
-
-#include "../snake.h"
-
-void snake_render_frame(snake_t* snake);
-
-#endif  // SNAKE_RENDER_H
